03f82d9c7ca6a8c727eaaee87ee1a60a7230da7a
Daniel Duarte Medeiros <duartedaniel.contato@gmail.com>
2025-03-12 22:13:08 -0300

    Add files via upload

---
+++ exercbd01.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13/03/2025 às 02:04
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `exercbd01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pet`
+--
+
+CREATE TABLE `pet` (
+  `NumRegistro` int(5) NOT NULL,
+  `Nome` varchar(80) DEFAULT NULL,
+  `Especie` varchar(25) DEFAULT NULL,
+  `Raca` varchar(30) DEFAULT NULL,
+  `Cor` varchar(40) DEFAULT NULL,
+  `Nascimento` datetime(6) DEFAULT NULL,
+  `Sexo` varchar(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `pet`
+--
+ALTER TABLE `pet`
+  ADD PRIMARY KEY (`NumRegistro`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ exercbd02.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13/03/2025 às 02:11
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `exercbd02`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `CodDepto` int(11) DEFAULT NULL,
+  `NomeDepto` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `CodFunc` int(11) DEFAULT NULL,
+  `NomeFunc` varchar(50) DEFAULT NULL,
+  `CodDepto` int(11) DEFAULT NULL,
+  `Ramal` int(11) DEFAULT NULL,
+  `Salario` double DEFAULT NULL,
+  `DataAdmissao` datetime DEFAULT NULL,
+  `DataCadastro` datetime DEFAULT NULL,
+  `Sexo` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
